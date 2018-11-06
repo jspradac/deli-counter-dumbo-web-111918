2d5d118d392a7e8c90345f6ae34ebaa47d995cd8
Samuel Prada <jspradac@gmail.com>
2018-11-06 18:15:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(queue, name)
 end 
 def now_serving(queue)
   if queue.length > 0 
-    puts "Now serving #{queue.shift}"
+    puts "Currently serving #{queue.shift}."
   else 
     puts "There is nobody waiting to be served!"
   end 
