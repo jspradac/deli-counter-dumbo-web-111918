67371dc5e8961010f13c134cee27d07692318e24
Samuel Prada <jspradac@gmail.com>
2018-11-06 18:00:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(queue)
   if queue.length > 0 
     message = "The line is currently:"
     queue.each do | guest_number |
-      message = message + 
+      message = message + " #{"
     puts queue 
   else 
     puts "The line is currently empty."
